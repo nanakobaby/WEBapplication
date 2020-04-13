323a69195de3388b971dcae2f1a37e704ffdd1b2
kannanako <n.k.0213.0529@gmail.com>
2020-04-13 13:00:37 +0900

    WEB課題途中経過

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'new.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')#<form action='indicate.cgi'> 〜 </form>の内部にある値を、indicate.rbに送信することができるようになります。
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.start
